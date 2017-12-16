e60c304e638056a8c876d70982e17e170f65593a
Varun Bansal <varun13168@iiitd.ac.in>
2017-12-16 14:51:07 +0530

    version 2.0

--- js/slider.js
+++ js/slider.js
@@ -14,7 +14,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  // var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -25,5 +25,5 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  // captionText.innerHTML = dots[slideIndex-1].alt;
 }
